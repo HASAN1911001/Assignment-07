2a569cc1e4361058b22b305d172ce9e87b0211c3
Mr. Zi <md.ziaulhasan00@gmail.com>
2022-09-03 14:15:27 +0600

    updated

--- js/index.js
+++ js/index.js
@@ -39,14 +39,14 @@ function newsD(news)
         newsDiv.classList.add('card');
 
         newsDiv.innerHTML = `
-            <div class="row g-0">
+            <div class="row g-0" style="height:300px;">
             <div class="col-md-4">
                 <img id="image" src="${news[n].thumbnail_url}" class="img-fluid rounded-start">
             </div>
             <div class="col-md-8">
                 <div class="card-body">
                 <h5 id ="title" class="card-title">${news[n].title}</h5>
-                <p id="text" class="card-text">${news[n].details}.slice(0, 300)</p>
+                <p id="text" class="card-text" style="white-space:nowrap;  overflow:hidden; text-overflow: ellipsis;">${news[n].details}.slice(0, 300)</p>
                 </div>
                 <div class="d-flex">
                     <div class="mr-auto p-2">
@@ -54,7 +54,7 @@ function newsD(news)
                     ${news[n].author.name}
                     </div> 
                     <p class="p-2">Views: ${news[n].total_view}</p>
-                    <button type="button" class="btn btn-primary ml-auto p-2">Primary</button>
+                    <button type="button" class="btn btn-primary" onclick=detailsNews('${news[n]._id}')>Primary</button>
 
                 </div>
             </div>
@@ -63,3 +63,15 @@ function newsD(news)
         newsContainer.appendChild(newsDiv);
     }
 }
+
+function detailsNews(news_id)
+{
+    const url = `https://openapi.programming-hero.com/api/news/category/${news_id}`;
+
+    fetch(url)
+        .then(response => response.json())
+        .then(json => json.data)
+        .then(data => details(data))
+}
+
+
